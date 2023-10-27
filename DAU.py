3b88bb4945a498f79e3ea51c2d5436b93a0c9011
Dudragon1 <126318755+Dudragon1@users.noreply.github.com>
2023-10-27 23:09:44 +0800

    Add files via upload

---
+++ DAU.py
@@ -0,0 +1,96 @@
+# --- Imports --- #
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+# from pdb import set_trace as stx
+
+##########################################################################
+def conv(in_channels, out_channels, kernel_size, bias=False, padding = 1, stride = 1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2), bias=bias, stride = stride)
+
+##########################################################################
+##---------- Spatial Attention ----------
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=False, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
+
+class spatial_attn_layer(nn.Module):
+    def __init__(self, kernel_size=5):
+        super(spatial_attn_layer, self).__init__()
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False)
+
+    def forward(self, x):
+        # import pdb;pdb.set_trace()
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = torch.sigmoid(x_out)  # broadcasting
+        return x * scale
+
+
+##########################################################################
+## ------ Channel Attention --------------
+class ca_layer(nn.Module):
+    def __init__(self, channel, reduction=8, bias=True):
+        super(ca_layer, self).__init__()
+        # global average pooling: feature --> point
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # feature channel downscale and upscale --> channel weight
+        self.conv_du = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=bias),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=bias),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+
+##########################################################################
+##---------- Dual Attention Unit (DAU) ----------
+class DAU(nn.Module):
+    def __init__(
+            self, n_feat, kernel_size=3, reduction=8,
+            bias=False, bn=False, act=nn.PReLU(), res_scale=1):
+        super(DAU, self).__init__()
+        modules_body = [conv(n_feat, n_feat, kernel_size, bias=bias), act, conv(n_feat, n_feat, kernel_size, bias=bias)]
+        self.body = nn.Sequential(*modules_body)
+
+        ## Spatial Attention
+        self.SA = spatial_attn_layer()
+
+        ## Channel Attention
+        self.CA = ca_layer(n_feat, reduction, bias=bias)
+
+        self.conv1x1 = nn.Conv2d(n_feat * 2, n_feat, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        res = self.body(x)
+        sa_branch = self.SA(res)
+        ca_branch = self.CA(res)
+        res = torch.cat([sa_branch, ca_branch], dim=1)
+        res = self.conv1x1(res)
+        res += x
+        return res
